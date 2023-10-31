e832d303dd57cd4976789710c5a61c5c446b89cb
Abdul Wajid <abdulwajidnv@gmail.com>
2023-10-31 08:09:16 +0530

    style: Reformat intendations to improve understanding

--- src/ui/src/components/chart/LineChart.tsx
+++ src/ui/src/components/chart/LineChart.tsx
@@ -1,22 +1,33 @@
 import React from "react";
-import {JinjatECharts, JinjatEChartsProps} from "@components/chart/JinjatECharts";
-
+import {
+  JinjatECharts,
+  JinjatEChartsProps,
+} from "@components/chart/JinjatECharts";
 
 interface JinjatLineChartsProps extends JinjatEChartsProps {
-    x: string,
-    y: string
+  x: string;
+  y: string;
 }
 
 export const JinjatLineChart: React.FC<JinjatLineChartsProps> = (props) => {
-    const options = props.options || {}
-    if(options.series != null) {
-        return <div>`options.series` is not null, please use `ECharts` component instead.</div>
-    }
-    options.series = [{ type: 'line', encode: {
-            x: props.x,
-            y: props.y
-        }}]
-    return <JinjatECharts {...props} options={options}/>
-}
+  const options = props.options || {};
+  if (options.series != null) {
+    return (
+      <div>
+        `options.series` is not null, please use `ECharts` component instead.
+      </div>
+    );
+  }
+  options.series = [
+    {
+      type: "line",
+      encode: {
+        x: props.x,
+        y: props.y,
+      },
+    },
+  ];
+  return <JinjatECharts {...props} options={options} />;
+};
 
-JinjatLineChart.displayName = "LineChart"
+JinjatLineChart.displayName = "LineChart";
--- src/ui/src/components/chart/index.ts
+++ src/ui/src/components/chart/index.ts
@@ -1,12 +1,10 @@
-import {JinjatECharts} from "@components/chart/JinjatECharts";
-import {JinjatLineChart} from "@components/chart/LineChart";
-import {JinjatKPI} from "@components/chart/JinjatKPI";
-import {JinjatDataset} from "@components/crud/utils";
+import { JinjatECharts } from "@components/chart/JinjatECharts";
+import { JinjatLineChart } from "@components/chart/LineChart";
+import { JinjatKPI } from "@components/chart/JinjatKPI";
+import { JinjatDataset } from "@components/crud/utils";
 
-
-
-const allComponents = {}
-allComponents[JinjatECharts.displayName] = JinjatECharts
-allComponents[JinjatLineChart.displayName] = JinjatLineChart
-allComponents[JinjatKPI.displayName] = JinjatKPI
-export default allComponents
+const allComponents = {};
+allComponents[JinjatECharts.displayName] = JinjatECharts;
+allComponents[JinjatLineChart.displayName] = JinjatLineChart;
+allComponents[JinjatKPI.displayName] = JinjatKPI;
+export default allComponents;
